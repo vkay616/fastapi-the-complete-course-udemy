e6e846e74902e9ae9bd6973daf47c0c5ac44e6d0
vkay616 <vinaykhanduri6@gmail.com>
2023-09-15 21:56:57 +0530

    created endpoint for delete request

--- project1/books.py
+++ project1/books.py
@@ -168,3 +168,11 @@ def update_book(updated_book = Body()):
     for i in range(len(BOOKS)):
         if BOOKS[i].get("id") == updated_book.get("id"):
             BOOKS[i] = updated_book
+
+
+@app.delete("/books/delete_book/{book_id}")
+def delete_book(book_id: int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get("id") == book_id:
+            BOOKS.pop(i)
+            break
