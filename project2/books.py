cb0fc39dc4a6e02ca407a420d2527c3777e037c9
vkay616 <vinaykhanduri6@gmail.com>
2023-09-16 14:14:00 +0530

    created endpoint for searching by year (assignment)

--- project2/books.py
+++ project2/books.py
@@ -10,13 +10,15 @@ class Book:
     author: str
     description: str
     rating: int
+    year: int
 
-    def __init__(self, id, title, author, description, rating) -> None:
+    def __init__(self, id, title, author, description, rating, year) -> None:
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.year = year
 
 
 class BookRequest(BaseModel):
@@ -25,6 +27,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str
     rating: int = Field(gt=-1, lt=11)
+    year: int = Field(gt=0, lt=2024)
 
     class Config:
         json_schema_extra = {
@@ -32,23 +35,24 @@ class BookRequest(BaseModel):
                 "title": "Book Title",
                 "author": "Author's Name",
                 "description": "Description of the Book",
-                "rating": 5
+                "rating": 5,
+                "year": 2023
             }
         }
 
 
 
 BOOKS = [
-    Book(1, 'Book1', 'Author1', 'Description1', 10),
-    Book(2, 'Book2', 'Author2', 'Description2', 9),
-    Book(3, 'Book3', 'Author3', 'Description3', 8),
-    Book(4, 'Book4', 'Author4', 'Description4', 7),
-    Book(5, 'Book5', 'Author5', 'Description5', 6),
-    Book(6, 'Book6', 'Author5', 'Description6', 5),
-    Book(7, 'Book7', 'Author4', 'Description7', 4),
-    Book(8, 'Book8', 'Author3', 'Description8', 3),
-    Book(9, 'Book9', 'Author2', 'Description9', 2),
-    Book(10, 'Book10', 'Author1', 'Description10', 1)
+    Book(1, 'Book1', 'Author1', 'Description1', 10, 2010),
+    Book(2, 'Book2', 'Author2', 'Description2', 9, 2011),
+    Book(3, 'Book3', 'Author3', 'Description3', 8, 2012),
+    Book(4, 'Book4', 'Author4', 'Description4', 7, 2013),
+    Book(5, 'Book5', 'Author5', 'Description5', 6, 2021),
+    Book(6, 'Book6', 'Author5', 'Description6', 5, 2023),
+    Book(7, 'Book7', 'Author4', 'Description7', 4, 2020),
+    Book(8, 'Book8', 'Author3', 'Description8', 3, 2008),
+    Book(9, 'Book9', 'Author2', 'Description9', 2, 2023),
+    Book(10, 'Book10', 'Author1', 'Description10', 1, 2021)
 ]
 
 def create_book_id(book: Book):
@@ -79,6 +83,16 @@ def get_book_by_rating(rating: int):
         if book.rating == rating:
             b.append(book)
     
+    return b
+
+
+@app.get("/books/search/")
+def get_book_by_year(year: int):
+    b = []
+    for book in BOOKS:
+        if book.year == year:
+            b.append(book)
+
     return b
 
 
