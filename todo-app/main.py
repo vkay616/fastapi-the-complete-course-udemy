45f6be5f9de934304f5bf57cdc2beb84acab2ace
vkay616 <vinaykhanduri6@gmail.com>
2023-09-20 21:21:42 +0530

    created endpoint for getting todo by its id

--- todo-app/main.py
+++ todo-app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, status, Path
 import models
 from models import Todos
 from database import engine, SessionLocal
@@ -20,5 +20,14 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 @app.get("/")
-def read_all(db: db_dependency):
+def get_all_todos(db: db_dependency):
     return db.query(Todos).all()
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+def get_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    else:
+        raise HTTPException(status_code=404, detail="Todo not found!")
