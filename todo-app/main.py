87803028bdef7d528ab06ecdd71e430305fa6cd0
vkay616 <vinaykhanduri6@gmail.com>
2023-09-20 21:13:18 +0530

    created endpoint for getting all todos from db

--- todo-app/main.py
+++ todo-app/main.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import models
-from database import engine
+from models import Todos
+from database import engine, SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
+
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/")
+def read_all(db: db_dependency):
+    return db.query(Todos).all()
