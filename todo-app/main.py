753c60f07bfd6bc317c6c90b269206438e2ea383
vkay616 <vinaykhanduri6@gmail.com>
2023-09-20 21:45:14 +0530

    created endpoints for post and put request

--- todo-app/main.py
+++ todo-app/main.py
@@ -4,6 +4,7 @@ from models import Todos
 from database import engine, SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
 
 
 app = FastAPI()
@@ -19,6 +20,12 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
 @app.get("/")
 def get_all_todos(db: db_dependency):
     return db.query(Todos).all()
@@ -31,3 +38,26 @@ def get_todo(db: db_dependency, todo_id: int = Path(gt=0)):
         return todo_model
     else:
         raise HTTPException(status_code=404, detail="Todo not found!")
+    
+
+@app.post("/todo/add", status_code=status.HTTP_201_CREATED)
+def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+
+    db.add(todo_model)
+    db.commit()
+
+     
+@app.put("/todo/edit/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+def edit_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int= Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail="Todo not found!")
+    else:
+        todo_model.title = todo_request.title
+        todo_model.description = todo_request.description
+        todo_model.priority = todo_request.priority
+        todo_model.complete = todo_request.complete
+
+        db.add(todo_model)
+        db.commit()
