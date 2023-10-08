7365b14274e3d7f78a672c03ea3e33823616a5d7
vkay616 <vinaykhanduri6@gmail.com>
2023-10-08 22:56:28 +0530

    changed from sqlite db to postgres db

--- todo-app/database.py
+++ todo-app/database.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from os import environ
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+SQLALCHEMY_DATABASE_URL = f'postgresql://postgres:{environ["POSTGRES_PASSWORD"]}@localhost/TodoApplicationDatabase'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
